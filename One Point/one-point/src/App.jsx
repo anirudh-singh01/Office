7731486f743baa9d23c938fd85a42f2a761708f9
anirudh-singh01 <anirudhsingh.ani321@gmail.com>
2025-09-03 09:30:29 +0530

    fix

--- One Point/one-point/src/App.jsx
+++ One Point/one-point/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Router>
       <div className="flex h-screen bg-gray-50">
